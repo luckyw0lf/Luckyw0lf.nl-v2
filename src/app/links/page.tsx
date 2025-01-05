610cad33e93cffb664b8c320511f96dd1044957f
Luckyw0lf <ruben@balduk.nl>
2025-01-05 18:52:43 +0100

    cleaned up classnames and added steam link

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -3,7 +3,7 @@ import Card from "../components/Card";
 export default function Links() {
     return (
         <div className="flex justify-center m-4 p-4">
-        <div className="grid grid-cols-1 grid-rows-3 place-items-center md:grid-cols-3 w-full 2xl:w-2/3 gap-4">
+        <div className="grid grid-cols-1 grid-rows-3 lg:grid-cols-3 lg:grid-rows-1 w-full 2xl:w-2/3 gap-4 pt-2">
             <Card 
             title="Discord"
             description="My personal Discord account."
@@ -17,13 +17,19 @@ export default function Links() {
             link="https://github.com/luckyw0lf"
             />
             <Card 
+            title="Steam"
+            description="ruben0770"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/1200px-Steam_icon_logo.svg.png"
+            link="https://steamcommunity.com/id/Ruben0770"
+            />
+            <Card 
             title="Buy Me A Coffee"
             description="Only if you want to."
             src="/buymeacoffee.png"
             link="https://buymeacoffee.com/luckyw0lf"
             />
             <Card 
-            title="E-mail"
+            title="E-Mail"
             description="mcluckyw0lf@gmail.com"
             src="/gmail.svg"
             link=""
